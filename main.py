7fbf880f40af1ce8e818438b837c29336cd772e2
Nicholas Klos <nicholask39@gmail.com>
2025-06-01 12:36:18 -0400

    Refactor SQL table creation for candidates, voters, and votes to improve readability and maintainability

--- main.py
+++ main.py
@@ -97,112 +97,41 @@ def create_tables(cur):
                 CREATE TABLE IF NOT EXISTS candidates (
                     candidate_id VARCHAR(255) PRIMARY KEY,
                     candidate_name VARCHAR(255),
-                    party_affiliation VARCHAR
-                (
-                    255
-                ),
+                    party_affiliation VARCHAR(255),
                     biography TEXT,
                     campaign_platform TEXT,
                     photo_url TEXT
                     )
                 """)
     cur.execute("""
-                CREATE TABLE IF NOT EXISTS voters
-                (
-                    voter_id
-                    VARCHAR
-                (
-                    255
-                ) PRIMARY KEY,
-                    voter_name VARCHAR
-                (
-                    255
-                ),
-                    date_of_birth VARCHAR
-                (
-                    255
-                ),
-                    gender VARCHAR
-                (
-                    255
-                ),
-                    nationality VARCHAR
-                (
-                    255
-                ),
-                    registration_number VARCHAR
-                (
-                    255
-                ),
-                    address_street VARCHAR
-                (
-                    255
-                ),
-                    address_city VARCHAR
-                (
-                    255
-                ),
-                    address_state VARCHAR
-                (
-                    255
-                ),
-                    address_country VARCHAR
-                (
-                    255
-                ),
-                    address_postcode VARCHAR
-                (
-                    255
-                ),
-                    email VARCHAR
-                (
-                    255
-                ),
-                    phone_number VARCHAR
-                (
-                    255
-                ),
-                    cell_number VARCHAR
-                (
-                    255
-                ),
+                CREATE TABLE IF NOT EXISTS voters(
+                    voter_id VARCHAR(255) PRIMARY KEY,
+                    voter_name VARCHAR(255),
+                    date_of_birth VARCHAR(255),
+                    gender VARCHAR(255),
+                    nationality VARCHAR(255),
+                    registration_number VARCHAR(255),
+                    address_street VARCHAR(255),
+                    address_city VARCHAR(255),
+                    address_state VARCHAR(255),
+                    address_country VARCHAR(255),
+                    address_postcode VARCHAR(255),
+                    email VARCHAR(255),
+                    phone_number VARCHAR(255),
+                    cell_number VARCHAR(255),
                     picture TEXT,
                     registered_age INTEGER
                     )
                 """)
     cur.execute("""
-                CREATE TABLE IF NOT EXISTS votes
-                (
-                    voter_id
-                    VARCHAR
-                (
-                    255
-                ) UNIQUE,
-                    candidate_id VARCHAR
-                (
-                    255
-                ),
+                CREATE TABLE IF NOT EXISTS votes(
+                    voter_id VARCHAR(255) UNIQUE,
+                    candidate_id VARCHAR(255),
                     voting_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                     vote INT DEFAULT 1,
-                    PRIMARY KEY
-                (
-                    voter_id,
-                    candidate_id
-                ),
-                    FOREIGN KEY
-                (
-                    voter_id
-                ) REFERENCES voters
-                (
-                    voter_id
-                ),
-                    FOREIGN KEY
-                (
-                    candidate_id
-                ) REFERENCES candidates
-                (
-                    candidate_id
-                )
+                    PRIMARY KEY(voter_id, candidate_id),
+                    FOREIGN KEY(voter_id) REFERENCES voters(voter_id),
+                    FOREIGN KEY(candidate_id) REFERENCES candidates(candidate_id)
                     )
                 """)
     logging.info("Database tables ensured to exist.")
